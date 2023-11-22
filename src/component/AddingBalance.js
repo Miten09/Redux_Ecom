246cc20692705fc1c632f44ff3250117b752eacb
Miten09 <mitensoni108@gmail.com>
2023-11-22 15:25:02 +0530

    Adding balance and deducting balance completed

--- src/component/AddingBalance.js
+++ src/component/AddingBalance.js
@@ -34,22 +34,53 @@ const AddingBalance = () => {
         value={Number(amount)}
         onChange={handleInputChange}
       />
-
       <div className="balance-buttons">
         <button onClick={() => setAmount(amount + 100)}>+ 100</button>
-        <button onClick={() => setAmount(amount - 100)}>- 100</button>
+        <button
+          onClick={() => {
+            if (amount >= 100) {
+              setAmount(amount - 100);
+            }
+          }}
+        >
+          - 100
+        </button>
       </div>
       <div className="balance-buttons">
         <button onClick={() => setAmount(amount + 200)}>+ 200</button>
-        <button onClick={() => setAmount(amount - 200)}>- 200</button>
+        <button
+          onClick={() => {
+            if (amount >= 200) {
+              setAmount(amount - 200);
+            }
+          }}
+        >
+          - 200
+        </button>
       </div>
       <div className="balance-buttons">
         <button onClick={() => setAmount(amount + 500)}>+ 500</button>
-        <button onClick={() => setAmount(amount - 500)}>- 500</button>
+        <button
+          onClick={() => {
+            if (amount >= 500) {
+              setAmount(amount - 500);
+            }
+          }}
+        >
+          - 500
+        </button>
       </div>
       <div className="balance-buttons">
         <button onClick={() => setAmount(amount + 1000)}>+ 1000</button>
-        <button onClick={() => setAmount(amount - 1000)}>- 1000</button>
+        <button
+          onClick={() => {
+            if (amount >= 1000) {
+              setAmount(amount - 1000);
+            }
+          }}
+        >
+          - 1000
+        </button>
       </div>
       <div className="balance-buttons but">
         <button
